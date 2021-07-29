96be9713288bf2242675d120fdb8720e642bd499
The-Hungry-Caterpiillar <ogata1991@gmail.com>
2021-07-28 18:04:53 -0700

    think complete, job still running

--- distribution_SLURM.sh
+++ distribution_SLURM.sh
@@ -12,10 +12,10 @@ plot_dir=/projects/bgmp/jogata/bioinformatics/bi622/Demux/distribution_plots
 conda activate bgmp_py39
 
 read1=$shared_dir/1294_S1_L008_R1_001.fastq.gz
-records=$(grep -c '^@K00337' $read1)
+records=$(gunzip -c $read1 |grep -c '^@K00337')
 
 /usr/bin/time -v \
-python $plot_dir/mean_quality_distribution.py \
+python mean_quality_distribution.py \
 -f $read1 \
 -l 101 \
 -nr $records \
@@ -24,10 +24,9 @@ python $plot_dir/mean_quality_distribution.py \
 
 
 index1=$shared_dir/1294_S1_L008_R2_001.fastq.gz
-records=$(grep -c '^@K00337' $index1)
 
 /usr/bin/time -v \
-python $plot_dir/mean_quality_distribution.py \
+python mean_quality_distribution.py \
 -f $index1 \
 -l 8 \
 -nr $records \
@@ -36,10 +35,9 @@ python $plot_dir/mean_quality_distribution.py \
 
 
 index2=$shared_dir/1294_S1_L008_R3_001.fastq.gz
-records=$(grep -c '^@K00337' $index2)
 
 /usr/bin/time -v \
-python $plot_dir/mean_quality_distribution.py \
+python mean_quality_distribution.py \
 -f $index2 \
 -l 8 \
 -nr $records \
@@ -48,10 +46,9 @@ python $plot_dir/mean_quality_distribution.py \
 
 
 read2=$shared_dir/1294_S1_L008_R4_001.fastq.gz
-records=$(grep -c '^@K00337' $read2)
 
 /usr/bin/time -v \
-python $plot_dir/mean_quality_distribution.py \
+python mean_quality_distribution.py \
 -f $read2 \
 -l 101 \
 -nr $records \
